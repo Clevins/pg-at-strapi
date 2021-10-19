d1515140d67079f2e3a12a203b5fc66bc0e145ab
Cormac Levins <cormac.levins@axonitsa.com>
2021-10-19 13:41:13 +0100

    init commit

---
+++ api/home/models/home.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {
+  lifecycles: {
+    afterCreate(data) {
+      data.video_shelves = "Some fixed name";
+    },
+  },
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4b595e9c-d0af-4355-8898-ac3acf592730'
+};
